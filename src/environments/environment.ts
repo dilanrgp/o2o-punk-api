246d10bc854afb9a03a2d87ec3cfa2eb7937e2dd
Dilan Gonzalez <dgonzalez@grupoambox.com>
2023-09-13 19:32:41 +0200

    Ajustando enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     baseUrl: 'https://api.punkapi.com/v2'
-}
+};
--- src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environments = {
-    production: false,
-    baseUrl: 'https://api.punkapi.com/v2'
-}
